9684f9285e5d8f112afea02814012da6201fe420
andrepardinho <nandre670@gmail.com>
2025-04-03 22:00:54 -0300

    date manipulation

--- src/andrePardinho/exercicios/datas/App.java
+++ src/andrePardinho/exercicios/datas/App.java
@@ -1,4 +1,21 @@
 package andrePardinho.exercicios.datas;
 
 public class App {
+    public static void main(String[] args) {
+
+        Data data1 = new Data();
+        System.out.println("Data atual: " + data1.dia + "/" + data1.mes + "/" + data1.ano);
+
+        Data data2 = new Data(19, 8, 2003);
+        System.out.println("Data especifica: " + data2.dia + "/" + data2.mes + "/" + data2.ano);
+
+        Data data3 = new Data("11/07/2001");
+        System.out.println("Data String: " + data3.dia + "/" + data3.mes + "/" + data3.ano);
+
+        //Teste data string com entrada do usuario
+
+        System.out.println("Data 1 e Data 2 são iguais? " + Data.IsEqual(data1, data2));
+
+
+    }
 }
--- src/andrePardinho/exercicios/datas/Data.java
+++ src/andrePardinho/exercicios/datas/Data.java
@@ -1,4 +1,64 @@
 package andrePardinho.exercicios.datas;
+import java.time.LocalDate;
 
 public class Data {
+    int dia;
+    int mes;
+    int ano;
+
+    //Construtor p/ data atual do SO
+    public Data() {
+        LocalDate dataAtual = LocalDate.now();
+        this.dia = dataAtual.getDayOfMonth();
+        this.mes = dataAtual.getMonthValue();
+        this.ano = dataAtual.getYear();
+    }
+
+    // Construtor p/ data de acordo com os parâmetros
+    public Data(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
+    // Construtor p/ data de acordo com a String
+    public Data(String data) {
+        String[] partes = data.split("/"); //
+
+        if (partes.length == 3) {
+            this.dia = Integer.parseInt(partes[0]);
+            this.mes = Integer.parseInt(partes[1]);
+            this.ano = Integer.parseInt(partes[2]);
+        } else {
+            System.out.println("Data inválida");
+        }
+
+    }
+
+
+    private boolean DataValida(Data data) {
+        if (data.mes < 1 || data.mes > 12 || data.dia < 1) {
+            return false;
+        }
+
+        int[] diasNoMes = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        if (anoBissexto(data.ano)) {
+            diasNoMes[1] = 29; // Fevereiro em ano bissexto
+        }
+
+        return data.dia <= diasNoMes[data.mes - 1];
+
+    }
+
+    private static boolean anoBissexto(int ano) {
+        return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
+    }
+
+
+    public static boolean IsEqual(Data data1, Data data2) {
+        return(data1.dia == data2.dia && data1.mes == data2.mes && data1.ano == data2.ano);
+    }
+
+
 }
