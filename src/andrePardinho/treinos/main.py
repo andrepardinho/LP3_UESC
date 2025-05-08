c9bf525ed7bbdfbd60ceba96045305045f92d6b8
andrepardinho <nandre670@gmail.com>
2025-05-08 19:00:38 -0300

    adding source files

---
+++ src/andrePardinho/treinos/main.py
@@ -0,0 +1,6 @@
+nome = input("Qual o seu nome? ")
+idade = int(input("Qual a sua idade? "))
+peso = float(input("Qual o seu peso? "))
+altura = int(input("Qual a sua altura? "))
+
+print(f"Olá {nome}, você tem {idade} anos, pesa {peso} kg e tem {altura} cm de altura.")
